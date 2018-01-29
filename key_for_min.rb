c7eafb2c6c41fb3d10d2bd0a7f71d78a49112034
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-29 13:10:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   low = 0 #Hash[*name_hash.first]
   name_hash.each do |name, value|
-    if value < low 
+    if value < low
       low = value
     end
   end
-  low    
+  low
 end
